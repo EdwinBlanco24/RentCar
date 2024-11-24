070f15fddfcbe869643bfdf7207bae1ac7dfd7af
EdwinBlanco24 <160681370+EdwinBlanco24@users.noreply.github.com>
2024-11-23 23:16:14 -0500

    Update rentcar.sql

--- back-end/rentcar.sql
+++ back-end/rentcar.sql
@@ -25,12 +25,12 @@ DROP TABLE IF EXISTS `clientes`;
 CREATE TABLE `clientes` (
   `cliente_id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `celular` varchar(11) NOT NULL,
+  `celular` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `estado_rg` bit(1) DEFAULT b'1',
   `fecha_rg` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `fecha_md` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`cliente_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES (1,'PPEREZ@GMAIL.COM','3002248773',_binary '','2024-10-19 18:57:46','2024-11-15 18:47:11'),(2,'string','string',_binary '','2024-10-26 17:20:15','2024-10-26 17:21:34'),(3,'SERVICIOS','6053745454',_binary '','2024-11-15 18:54:55','2024-11-15 18:55:16');
+INSERT INTO `clientes` VALUES (1,'MARIA','3124569901',_binary '','2024-11-24 03:35:27',NULL),(2,'MARI','3152276544',_binary '','2024-11-24 03:56:39',NULL),(3,'Edwin','3137335534',_binary '','2024-11-24 04:10:39',NULL);
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,12 +53,12 @@ DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `usuario_id` int NOT NULL AUTO_INCREMENT,
   `correo` varchar(50) NOT NULL,
-  `contraseña` varchar(50) NOT NULL,
+  `contraseña` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `estado_rg` bit(1) DEFAULT b'1',
   `fecha_rg` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `fecha_md` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`usuario_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'H@TEST','H123',_binary '','2024-10-10 15:20:02','2024-11-13 18:59:50'),(2,'string@d','string',_binary '','2024-11-06 15:47:41','2024-11-13 19:58:18'),(3,'ahjjkds@te','121212',_binary '','2024-11-13 19:43:17',NULL),(4,'ahjjkds@test.com','1234555',_binary '','2024-11-13 19:44:31','2024-11-13 19:47:31'),(5,'ana@gmail.com','898989',_binary '','2024-11-13 19:46:29','2024-11-13 20:00:07'),(6,'asa','asdada',_binary '','2024-11-13 20:16:27',NULL);
+INSERT INTO `usuarios` VALUES (1,'ejblancov24@ul.edu.co','$2b$12$LafD/EoraBHDssEz0me42e3FNFkv3O7O9rweuax1wfq4.NvnQ.jYy',_binary '','2024-09-24 20:40:07','2024-10-24 01:41:30'),(3,'BRAYAN@test.com','$2b$12$GM93oKe1IdskQo7fVvIZ8udql5YdJt.gxWEbLyofq7mAJTv4DaTW.',_binary '','2024-11-24 00:58:16',NULL),(15,'MESTRADA@gmail.com','$2b$12$9WtNqxr/32E.91H3GpeVY.1YqBZ8QZ/U7iqaOq/rCc3esEe2OUIGO',_binary '','2024-10-11 00:31:42',NULL),(16,'BRAYAN5@test.com','$2b$12$hdqxfGi5DHmiSi16fLqHEOv.yOjfPXLTOUGO8jgVTT1C4wOR8EQDS',_binary '','2024-11-24 01:53:27',NULL);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-23 12:27:00
+-- Dump completed on 2024-11-23 23:14:48
